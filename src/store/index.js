a1f1643f255bc3cb46edd179eff1fa2cd2e02717
temcha <itemchur@gmail.com>
2022-05-01 15:49:58 +0300

    add logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,19 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { logger } from 'redux-logger';
 import { rootReducer } from './root-reducer';
+
+const middleware = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
 
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 export { store };
